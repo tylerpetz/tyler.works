4f4c261fc89761437f438261b9260fb66c8e31ff
tylerpetz <tylerpetz@gmail.com>
2018-10-29 11:54:33 -0400

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1 class="site-title">
+      <nuxt-link
+        :to="{ path: '/' }"
+        class="site-title__link">
+        Nuxt Demo
+      </nuxt-link>
+    </h1>
+
+    <section class="main-content columns">
+
+      <div class="container column ">
+        <nuxt />
+      </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: 'Home', icon: 'home', to: { name: 'index' } },
+        { title: 'Inspire', icon: 'lightbulb', to: { name: 'inspire' } }
+      ]
+    };
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h2>Latest posts</h2>
+    <ul v-if="posts">
+      <li
+        v-for="(post, index) in posts"
+        :key="index">
+        <nuxt-link :to="post.fields.slug">
+          {{ post.fields.title }}
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  computed: {
+    posts() {
+      return this.$store.state.posts.posts;
+    }
+  },
+  async fetch({ store, params }) {
+    await store.dispatch('posts/getPosts', params.slug);
+  }
+};
+</script>
