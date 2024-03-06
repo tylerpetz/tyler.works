e90b0f2b127ba505886d718d9f46aac50d818a8a
Tyler Petz <tylerpetz@gmail.com>
2024-03-06 16:34:50 -0700

    updated cv

--- pages/cv.vue
+++ pages/cv.vue
@@ -15,10 +15,10 @@ export default {
               place: 'Remote',
               time: '2022 - 2024',
               bullets: [
-                'Worked with my team of 3 to develop a high performance web application using Vue and Nuxt for RE/MAX, a leading real estate franchise.',
+                'Worked with my team of 3 to develop a high performance web application using Vue and Nuxt for RE/MAX, resulting in 70% faster page loads on key pages.', // 3.5s to 1s
                 'Contributed to the map page integration, leveraging Google Maps API to provide users with an interactive map for searching real estate listings.',
-                'Translated Figma and InVision designs into reusable components, used Tailwind CSS to maintain a consistent UI and a small CSS footprint.',
-                'Established and maintained an integration test suite using Cypress, reducing visual and functional regression in our applications.',
+                'Translated Figma and InVision designs into reusable components in a design system using Storybook to maintain a consistent UI and a small CSS footprint.',
+                'Established and maintained an integration test suite using Cypress, resulting in a 60% decrease in visual and functional regression issues in our application.', // 110 issues to 45 issues
                 'Implemented Sentry to effectively track and address issues, bugs, and performance.',
                 'Aided in the development of an updated RE/MAX website with React, Next.js, and TypeScript to bring various applications under one codebase.',
               ],
@@ -30,9 +30,9 @@ export default {
               bullets: [
                 'Led a team of 11 Vue.js developers, aiding with collaboration and delivering high-quality frontend solutions across 5 different product teams.',
                 'Conducted resume reviews and interviews for frontend and backend developers, presented candidates to upper management.',
-                'Conducted code reviews to ensure adherence to industry best practices and coding standards.',
-                'Mentored and guided junior developers, facilitating their professional growth and the team\'s skill enhancement.',
-                'Updated and helped maintain our CI/CD deployment process, ensuring painless and timely delivery of software updates and features.',
+                'Performed daily code reviews to ensure adherence to industry best practices and coding standards, reviewing over 300 changes yearly.',
+                'Mentored and guided 2 junior developers, facilitating their professional growth and the team\'s skill enhancement.',
+                'Revamped and helped maintain our CI/CD deployment process, resulting in a 65% reduction in deployment time, and enabling us to deploy changes daily.', // 25 mins to 9 mins
                 'Proactively optimized site speed and performance, resulting in a notable reduction in page load times and enhanced user experience.',
               ],
             },
@@ -41,9 +41,9 @@ export default {
               place: 'On-site',
               time: '2019 - 2020',
               bullets: [
-                'Contributed to the development of a comprehensive suite for RE/MAX agents using Vue.js, allowing them to manage leads, track active deals, and build their online presence.',
+                'Contributed to the development of a comprehensive suite for RE/MAX agents using Vue.js, allowing them to manage leads, track active deals, and build their online presence. This led to a 67% increase in lead conversion rates for agents.', // 15% to 25%
                 'Worked with API developers to build data-intensive workflows, including front and backend validation, sorting, filtering, pagination, and searching.',
-                'Engineered a customizable website builder integrated with a robust CMS using Vue and Nuxt, giving agents the ability to personalize the display of their listings and services.',
+                'Engineered a customizable website builder integrated with a robust CMS using Vue and Nuxt, giving agents the ability to personalize the display of their listings and services, resulting in over 12,000 personalized web pages built for our agents.',
                 'Implemented a centralized system for managing contacts, calendars, and reminders, fostering better organization and timely follow-ups for real estate agents.',
                 'Collaborated with cross-functional teams to gather requirements and continuously enhanced the tooling suite.',
               ],
@@ -61,9 +61,9 @@ export default {
             'Designed and built end-to-end website solutions for various clients.',
             'Worked with Vue.js and Nuxt.js to craft dynamic and responsive user interfaces, ensuring an engaging and seamless user experience across various projects.',
             'Utilized Shopify to create e-commerce solutions, integrating custom features to meet client-specific requirements.',
-            'Managed hosting some websites on AWS, helped moved clients off of pricy hosting services and dedicated servers.',
+            'Managed hosting some websites on AWS, helped moved clients off of pricy hosting services and dedicated servers, saving my clients an average of 83%.', // 120/mo to 20/mo
             'Deployed websites using Netlify, optimizing the CI/CD pipeline for quick and easy project delivery.',
-            'Took care of domain management, including registration, configuration, and DNS setup',
+            'Took care of domain management, including registration, configuration, and DNS setup.',
           ],
           tools: ['Vue/Nuxt', 'Netlify', 'Prisma', 'Shopify', 'AWS Lambda', 'Express', 'Mongoose']
         },
@@ -76,11 +76,11 @@ export default {
           companyUrl: 'https://strivemedia.com',
           time: '2015 - 2019',
           bullets: [
-            'Developed frontends for a high-traffic web publisher covering travel, couponing, and automotive news.',
+            'Developed frontends for a high-traffic web publisher covering travel, couponing, and automotive news, serving over 15 million annual visitors.',
             'Worked with designers to create visually appealing and user-friendly websites.',
             'Engineered custom ad integrations and workflows for travel deals to build revenue.',
             'Leveraged Google Analytics to collect data, allowing the team to refine content strategy.',
-            'Built personalized email campaigns with MailChimp for continued communication with subscribers.',
+            'Built personalized email campaigns with MailChimp for continued communication with over 1.2 million subscribers.',
           ],
           tools: ['Wordpress', 'Laravel', 'Vagrant', 'SCSS']
         },
@@ -93,7 +93,7 @@ export default {
           companyUrl: 'https://e11group.com/',
           time: '2012 - 2015',
           bullets: [
-            'Translated Photoshop layouts into themes for WordPress, optimizing them for various screen sizes and devices.',
+            'Translated Photoshop layouts into themes for WordPress for over 25 businesses, optimizing them for various screen sizes and devices.',
             'Ensured the maintainability of themes and plugins, facilitating easy content updates and future modifications for clients.',
             'Maintained a client-focused approach, making iterative improvements to meet the evolving needs of each business.',
             'Contributed to the growth of local businesses by providing them with an online presence that effectively showcased their products and services.'
@@ -110,11 +110,11 @@ export default {
           title: 'Hobbies',
           items: ['Collecting Vinyl', 'Building Mechanical Keyboards', 'Coffee', 'Live Concerts', 'PC Gaming', 'Traveling']
         },
-        {
-          title: 'Professional References',
-          hidden: true,
-          items: ['Sean O\'Toole - 12344325 - Homie']
-        }
+        // {
+        //   title: 'Professional References',
+        //   hidden: true,
+        //   items: ['Sean O\'Toole - 12344325 - Homie']
+        // }
       ]
     }
   }
@@ -124,10 +124,10 @@ export default {
 <template>
   <section class="mb-24 flex flex-col lg:flex-row lg:space-x-6 print:mb-0 print:text-black">
     <div class="w-full lg:w-2/3 print:w-full">
-      <h1 class="text-2xl text-theme-text print:text-black font-semibold">
+      <h1 class="text-2xl text-theme-text print:text-black font-semibold print:-mb-1">
         Tyler Petz
       </h1>
-      <h2 class="text-lg print:text-base text-theme-text-d print:text-black mb-8 print:mb-8">
+      <h2 class="text-lg print:text-base text-theme-text-d print:text-black mb-8 print:mb-4">
         <em class="hidden print:inline">Denver, Colorado</em>
         <em class="hidden print:block text-sm">tylerpetz@gmail.com - <a
           href="https://tyler.works"
@@ -139,12 +139,12 @@ export default {
           class="underline"
         >linkedin.com/in/tyler-petz/</a></em>
       </h2>
-      <h1 class="font-semibold hidden print:block mb-4">
+      <!-- <h1 class="font-semibold hidden print:block mb-4">
         Summary of Qualifications
       </h1>
       <p class="hidden print:block mb-6 text-sm">
         With over 5 years of industry experience in web development, I bring a comprehensive skill set to the table. My expertise lies in HTML, CSS, JavaScript, and Vue.js, allowing me to build responsive web applications and implement pixel-perfect designs effectively. I have a proven track record of collaborating seamlessly with cross-functional teams and stakeholders, ensuring the delivery of high-quality solutions. Alongside my strong analytical and problem-solving abilities, I possess a commitment to continuous learning and improvement. With experience in RESTful APIs, TypeScript, and other relevant technologies, I am well-equipped to contribute to the development of scalable web applications. Thriving in fast-paced environments, I maintain a high level of quality and attention to detail in all my endeavors.
-      </p>
+      </p> -->
       <h1 class="font-semibold hidden print:block mb-0">
         Professional Experience
       </h1>
@@ -160,7 +160,7 @@ export default {
           <section
             v-for="role in job.roles"
             :key="role.title"
-            class="mb-2 print:mb-4"
+            class="mb-2 print:mb-2"
           >
             <h3
               class="text-lg lg:text-xl print:text-base print:inline"
@@ -168,12 +168,12 @@ export default {
               {{ role.title }} - {{ role.place }} <span class="print:inline hidden">-</span>
             </h3>
             <h4
-              class="mb-4 italic print:inline"
+              class="mb-4 italic print:inline print:text-base"
             >
               {{ role.time }}
             </h4>
             <ul
-              class="list-disc mb-6 pl-6 print:mb-2"
+              class="list-disc mb-6 pl-6 print:mb-0"
             >
               <li
                 v-for="(bullet, index) in role.bullets"
@@ -219,7 +219,7 @@ export default {
           >
             {{ job.title }} - {{ job.place }} <span class="print:inline hidden">-</span>
           </h3>
-          <h4 class="mb-4 italic print:inline">
+          <h4 class="mb-4 italic print:inline print:text-base">
             {{ job.time }}
           </h4>
           <ul class="list-disc mb-6 pl-6 print:mb-2">
